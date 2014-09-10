1ff89c1f1e279f33b7f53474bfc3f263ae59f3cc
Tomer Chachamu <tomer@gambitresearch.com>
2014-09-10 18:21:04 +0100

    v1.3.0-local+sha.7129343

--- angular.js
+++ angular.js
@@ -1,5 +1,5 @@
 /**
- * @license AngularJS v1.3.0-local+sha.375c907
+ * @license AngularJS v1.3.0-local+sha.7129343
  * (c) 2010-2014 Google, Inc. http://angularjs.org
  * License: MIT
  */
@@ -71,7 +71,7 @@ function minErr(module, ErrorConstructor) {
       return match;
     });
 
-    message = message + '\nhttp://errors.angularjs.org/1.3.0-local+sha.375c907/' +
+    message = message + '\nhttp://errors.angularjs.org/1.3.0-local+sha.7129343/' +
       (module ? module + '/' : '') + code;
     for (i = 2; i < arguments.length; i++) {
       message = message + (i == 2 ? '?' : '&') + 'p' + (i-2) + '=' +
@@ -2122,7 +2122,7 @@ function setupModuleLoader(window) {
  * - `codeName` – `{string}` – Code name of the release, such as "jiggling-armfat".
  */
 var version = {
-  full: '1.3.0-local+sha.375c907',    // all of these placeholder strings will be replaced by grunt's
+  full: '1.3.0-local+sha.7129343',    // all of these placeholder strings will be replaced by grunt's
   major: 1,    // package task
   minor: 3,
   dot: 0,
@@ -12821,10 +12821,9 @@ function $RootScopeProvider(){
       },
 
       /**
-       * @ngdoc function
+       * @ngdoc method
        * @name ng.$rootScope.Scope#$digestLimit
-       * @methodOf ng.$rootScope.Scope
-       * @function
+       * @kind function
        *
        * @description
        * By default, all the watchers are called every time {@link ng.$rootScope.Scope#methods_$digest $digest}
